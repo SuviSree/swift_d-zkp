a5a627d35d2a5248d15a1e4d05cdd857908d6e41
SuviSree <suvasri48@gmail.com>
2022-06-01 10:54:24 +0530

    cleansing 2

--- src/motioncore/crypto/sharing_randomness_generator.h
+++ src/motioncore/crypto/sharing_randomness_generator.h
@@ -1,26 +1,3 @@
-// MIT License
-//
-// Copyright (c) 2019 Oleksandr Tkachenko
-// Cryptography and Privacy Engineering Group (ENCRYPTO)
-// TU Darmstadt, Germany
-//
-// Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files (the "Software"), to deal
-// in the Software without restriction, including without limitation the rights
-// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-// copies of the Software, and to permit persons to whom the Software is
-// furnished to do so, subject to the following conditions:
-//
-// The above copyright notice and this permission notice shall be included in all
-// copies or substantial portions of the Software.
-//
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-// SOFTWARE.
 
 #pragma once
 
@@ -146,7 +123,7 @@ class SharingRandomnessGenerator {
     // Pre-initialize output vector
     std::vector<T> results(num_of_gates);
     GetUnsigned(gate_id, num_of_gates, results.data());
-    std::cout<<" results.size() = " << results.size() << "sizeof(results)" << sizeof(results) <<std::endl; 
+    std::cout<<" results.size() = " << results.size() << "sizeof(results)" << sizeof(results) <<std::endl;
     return results;
   }
 
